79e3edf11200c639316244a5ddf047ee43effa38
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-04 21:05:37 +0300

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,92 @@
+num = 5
+print(num)
+num = 6 + num
+print(num)
+print(2, 4, 8)
+
+
+print("int = " , 4)
+print("float = " , 3.0)
+print("bool = " , True,False)
+print("str = " , 'hello',"5")
+
+
+print("2 + 2 =", 2 + 2)
+print("2 - 2 =",2 - 2)
+print("2 / 2 =",2 / 2)
+print("2 * 2 =",2 * 3)
+print("2 ** 2 =",2 ** 3)
+
+
+
+num_2 = 4 * (2 + 3)
+print(num_2)
+
+
+
+
+int_num = 8
+print(int_num)
+
+qw4er = 8
+print(qw4er)
+
+intNum = 8
+print(intNum)
+
+
+
+
+num_3 = 7
+print(type(num_3))
+num_3 = "7"
+print(type(num_3))
+
+
+print("type int = " , type(4))
+print("type float = " , type(3.0))
+print("type bool = " , type(True))
+print("type str = " , type('hello'))
+
+
+
+str_int = "5"
+print(str_int * 3)
+print(type(str_int))
+
+str_int = int(str_int)
+
+print(str_int * 3)
+print(type(str_int))
+
+
+num_flo_int = 7.9
+print(int(num_flo_int))
+
+
+num_bool = True
+print(int(num_bool))
+num_bool = str(num_bool)
+print(num_bool)
+print(type(num_bool))
+
+
+str_num = True
+str_num = str(str_num)
+print(str_num)
+print(type(str_num))
+
+
+
+num_inp = int(input("введите int__"))
+# num_inp = int(num_inp)
+print("num_inp = ", num_inp * 4)
+
+print(float("y"))
+
+
+
+
+
+
+
