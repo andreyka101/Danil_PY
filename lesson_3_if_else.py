6f423e25613f708c2c26d4ea16ae2c646a92b4c1
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-25 21:05:12 +0300

    efefefff

---
+++ lesson_3_if_else.py
@@ -0,0 +1,48 @@
+# num = 1
+# if(num == 1):
+#     print("ok if")
+
+# print("end")
+# print(num == 1)
+
+# # True - 
+
+# print("2 == 2  =" , 2 == 2)
+# print("3 != 2  =" , 3 != 2)
+# print("4 < 2  =" , 4 < 2)
+# print("4 > 2  =" , 4 > 2)
+# print("4 <= 2  =" , 4 <= 2)
+# print("4 >= 2  =" , 4 >= 2)
+
+# print("2 < 2  =" , 2 < 2)
+# print("2 <= 2  =" , 2 <= 2)
+
+# if(num != 1):
+#     print("ok if 22")
+#     print("ok if 22")
+#     print("ok if 22")
+#     print("ok if 22")
+#     print("ok if 22")
+
+
+
+
+# num_2 = 5
+# if(num_2 < 3):
+#     print("ok if")
+# else:
+#     print("not else")
+
+
+
+# if(True):
+#     print("if(True)")
+
+
+
+# num_bool = True
+if(False and True):
+    print("if(True)")
+print(True and True)
+
+    
